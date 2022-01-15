39c8b24b9eacabbde1bb7ea3f113e88f569b5fef
Maxime Violi <maxime.violi@gmail.com>
2022-01-15 11:27:43 +0100

    fix header + add Login component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,7 @@
-import {
-  Box,
-  Button,
-  Flex,
-  FormControl,
-  FormLabel,
-  FormErrorMessage,
-  FormHelperText,
-  Heading,
-  Input,
-  useColorMode,
-} from "@chakra-ui/react";
+import Login from "./Login";
 
 function App() {
-  const { colorMode, toggleColorMode } = useColorMode();
-
-  return (
-    <Box p="1%">
-      <header>
-        <Box>
-          <Box>
-            <Heading>Easy Weather</Heading>
-          </Box>
-          <Box>
-            <Button onClick={toggleColorMode}>
-              {colorMode === "light" ? "Dark Mode" : "Light Mode"}
-            </Button>
-          </Box>
-        </Box>
-      </header>
-      <FormControl py="5px" px="15%">
-        <FormLabel htmlFor="email">Username</FormLabel>
-        <Input id="username" type="username" placeholder="Username" />
-      </FormControl>
-      <FormControl py="5px" px="15%">
-        <FormLabel htmlFor="email">Email address</FormLabel>
-        <Input id="email" type="email" placeholder="username@email.com" />
-        {/* <FormHelperText>We'll never share your email.</FormHelperText> */}
-      </FormControl>
-    </Box>
-  );
+  return <Login />;
 }
 
 export default App;
--- src/Login.js
+++ src/Login.js
@@ -1,20 +1,43 @@
 import {
+  Box,
+  Button,
+  Flex,
   FormControl,
   FormLabel,
+  FormErrorMessage,
   FormHelperText,
+  Heading,
   Input,
+  useColorMode,
 } from "@chakra-ui/react";
-import React from "react";
 
 function Login() {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <div>
-      <FormControl>
-        <FormLabel htmlFor="email">Email address</FormLabel>
-        <Input id="email" type="email" />
-        <FormHelperText>We'll never share your email.</FormHelperText>
+    <Box p="1%">
+      <header>
+        <Box display="flex" justifyContent="space-between">
+          <Box></Box>
+          <Box>
+            <Heading>Easy Weather</Heading>
+          </Box>
+          <Box>
+            <Button onClick={toggleColorMode}>
+              {colorMode === "light" ? "Dark Mode" : "Light Mode"}
+            </Button>
+          </Box>
+        </Box>
+      </header>
+      <FormControl py="5px" px="15%">
+        <FormLabel htmlFor="email">Username</FormLabel>
+        <Input id="username" type="username" placeholder="Username" />
       </FormControl>
-    </div>
+      <FormControl py="5px" px="15%">
+        <FormLabel htmlFor="email">Email address</FormLabel>
+        <Input id="email" type="email" placeholder="username@email.com" />
+        {/* <FormHelperText>We'll never share your email.</FormHelperText> */}
+      </FormControl>
+    </Box>
   );
 }
 
